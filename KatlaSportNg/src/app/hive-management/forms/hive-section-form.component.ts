f8c89c5ddca37b07c8783e76f4133541cd69ae3f
Vsevolod <vsevolod.gordienko99@gmail.com>
2019-01-27 19:25:48 +0300

    Merge step14. Add the ability to create a new section of the hive, edit and delete the existing section.

--- KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HiveSectionService } from '../services/hive-section.service';
+import { HiveSection } from '../models/hive-section';
+import { HiveListItem } from '../models/hive-list-item';
+import { HiveService } from '../services/hive.service';
 
 @Component({
   selector: 'app-hive-section-form',
@@ -7,8 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiveSectionFormComponent implements OnInit {
 
-  constructor() { }
+  hiveSection = new HiveSection(0, "", "", false, "", 0);
+  existed = false;
+  hives: HiveListItem[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private hiveSectionService: HiveSectionService,
+    private hiveService: HiveService
+  ) { }
 
   ngOnInit() {
+    this.hiveService.getHives().subscribe(c => this.hives = c);
+    this.route.params.subscribe(p => {
+      this.hiveSection.storeHiveId = p['hiveId'];
+      if (p['id'] === undefined) {
+        return;
+      }
+      this.hiveSectionService.getHiveSection(p['id']).subscribe(h => this.hiveSection = h);
+      this.existed = true;
+    });
+  }
+
+  onCancel() {
+    this.navigateToSections();
+  }
+
+  navigateToSections() {
+    this.router.navigate([`/hive/${this.hiveSection.storeHiveId}/sections`]);
+  }
+  
+  onSubmit() {
+    if (this.existed) {
+      this.hiveSectionService.updateHiveSection(this.hiveSection).subscribe(c => this.navigateToSections());
+    } else {
+      this.hiveSectionService.addHiveSection(this.hiveSection).subscribe(c => this.navigateToSections());
+    }
+  }
+
+  onDelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, true).subscribe(c => this.hiveSection.isDeleted = true);
+  }
+
+  onUndelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, false).subscribe(c => this.hiveSection.isDeleted = false);
+  }
+
+  onPurge() {
+    this.hiveSectionService.deleteHiveSection(this.hiveSection.id).subscribe(c => this.navigateToSections());
   }
 }
